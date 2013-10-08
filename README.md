1fc01b4c893d977c36f90f1e2fd4bc7d84ea34a5
Cyrille Rossant <cyrille.rossant@gmail.com>
2013-10-08 15:40:42 +0200

    First commit.

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-ipycache
-========
+%%cache cell magic
+==================
 
-Defines a %%cache cell magic in the notebook to persistent-cache results of long-lasting computations
+Defines a %%cache cell magic in the IPython notebook to cache results of long-lasting computations in a persistent pickle file. Useful when some computations in a notebook are long and you want to easily save the results in a file.
+
+
+Installation
+------------
+
+  * `pip install ipycache`
+  
+  
+Usage
+-----
+  
+  * In IPython:
+  
+        %load_ext ipycache
+  
+  * Then, create a cell with:
+  
+        %%cache var1 var2 --to=mycache.pkl
+        var1 = 1
+        var2 = 2
+
+  * When you execute this cell the first time, the code is executed, and the variables `var1` and `var2` are saved in `mycache.pkl` in the current directory. When you execute this cell again, the code is skipped and the variables are loaded from the file. Use the `--force` or `-f` option to force the cell's execution and overwrite the file.
+
+
+
---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="ipycache",
+    version="0.1",
+    author="Cyrille Rossant",
+    author_email="rossant@github",
+    description=(("Defines a %%cache cell magic in the IPython notebook to "
+                   "cache results of long-lasting computations in a persistent"
+                   "pickle file.")),
+    license="BSD",
+    keywords="ipython notebook cache",
+    url="http://packages.python.org/ipycache",
+    packages=['cachemagic'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
+
