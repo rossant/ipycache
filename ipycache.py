8d639d9b3af55ad19a1a469405cc3a703786373a
Cyrille Rossant <cyrille.rossant@gmail.com>
2013-10-09 12:44:51 +0200

    Added test notebook. Moved the conditions to a function. Added unit tests for conditions.

--- ipycache.py
+++ ipycache.py
@@ -46,6 +46,15 @@ def clean_vars(vars):
     """Clean variable names, removing accidental commas, etc."""
     return sorted(map(clean_var, vars))
 
+def do_save(path, force=False, read=False):
+    """Return True or False whether the variables need to be saved or not."""
+    if force and read:
+        raise ValueError(("The 'force' and 'read' options are "
+                          "mutually exclusive."))
+         
+    # Execute the cell and save the variables.
+    return force or (not read and not os.path.exists(path))
+    
 def load_vars(path, vars):
     """Load variables from a cPickle file.
     
@@ -61,14 +70,18 @@ def load_vars(path, vars):
     """
     with open(path, 'rb') as f:
         # Load the variables from the cache.
-        cache = cPickle.load(f)
+        try:
+            cache = cPickle.load(f)
+        except EOFError as e:
+            raise IOError(e.message)
         
         # Check that all requested variables could be loaded successfully
         # from the cache.
         missing_vars = list(set(vars) - set(cache.keys()))
         if missing_vars:
             raise ValueError(("The following variables could not be loaded "
-                "from the cache: {0:s}").format(', '.join(missing_vars)))
+                "from the cache: {0:s}").format(
+                ', '.join(["'{0:s}'".format(var) for var in missing_vars])))
         
         return cache
 
@@ -83,6 +96,41 @@ def save_vars(path, vars_d):
     """
     with open(path, 'wb') as f:
         cPickle.dump(vars_d, f)
+    
+def cache(cell, path, vars=[],
+          # HACK: this function implementing the magic's logic is testable
+          # without IPython, by giving mock functions here instead of IPython
+          # methods.
+          ip_user_ns={}, ip_run_cell=None, ip_push=None,
+          force=False, verbose=True, read=False):
+    
+    if not path:
+        raise ValueError("The path needs to be specified as a first argument.")
+        
+    if do_save(path, force=args.force, read=args.read):
+        ip.run_cell(cell)
+        # Create the cache from the namespace.
+        try:
+            cache = {var: ip_user_ns[var] for var in vars}
+        except KeyError:
+            raise ValueError(("Variable '{0:s}' could not be found in the "
+                              "interactive namespace").format(var))
+        # Save the cache in the pickle file.
+        save_vars(path, cache)
+        if verbose:
+            print("Saved variables {0:s} to file '{1:s}'.".format(
+                ', '.join(vars), path))
+        
+    # If the cache file exists, and no --force mode, load the requested 
+    # variables from the specified file into the interactive namespace.
+    else:
+        # Load the variables from cache in inject them in the namespace.
+        ip.push(load_vars(path, vars))
+        
+        if verbose:
+            print(("Skipped the cell's code and loaded variables {0:s} "
+                   "from file '{1:s}'.").format(', '.join(vars), path))
+        
     
 
 #------------------------------------------------------------------------------
@@ -111,12 +159,17 @@ class CacheMagics(Magics, Configurable):
         '-f', '--force', action='store_true', default=False,
         help="Force the cell's execution and save the variables."
     )
+    @magic_arguments.argument(
+        '-r', '--read', action='store_true', default=False,
+        help=("Always read from the file and prevent the cell's execution, "
+              "raising an error if the file does not exist.")
+    )
     @cell_magic
     def cache(self, line, cell):
         """Cache user variables in a file, and skip the cell if the cached
         variables exist.
         
-        Usage::
+        Usage:
         
             %%cache myfile.pkl var1 var2
             # If myfile.pkl doesn't exist, this cell is executed and 
@@ -131,35 +184,16 @@ class CacheMagics(Magics, Configurable):
         args = magic_arguments.parse_argstring(self.cache, line)
         code = cell if cell.endswith('\n') else cell+'\n'
         path = args.to[0]
-        verbose = args.verbose
         vars = clean_vars(args.vars)
         
-        if not path:
-            raise ValueError("The path needs to be specified as a first argument.")
-            
-        # If the cache file exists, and no --force mode, load the requested 
-        # variables from the specified file into the interactive namespace.
-        if os.path.exists(path) and not args.force:
-            if verbose:
-                print(("Skipping the cell's code and loading variables {0:s} "
-                       "from file '{1:s}'.").format(', '.join(vars), path))
-            # Load the variables from cache in inject them in the namespace.
-            ip.push(load_vars(path, vars))
-            
-        # Otherwise, execute the cell and save the variables.
-        else:
-            ip.run_cell(cell)
-            # Create the cache from the namespace.
-            try:
-                cache = {var: ip.user_ns[var] for var in vars}
-            except KeyError:
-                raise ValueError(("Variable '{0:s}' could not be found in the "
-                                  "interactive namespace").format(var))
-            # Save the cache in the pickle file.
-            save_vars(path, cache)
-            if verbose:
-                print("Saved variables {0:s} to file '{1:s}'.".format(
-                    ', '.join(vars), path))
+        cache(cell, path, vars=vars, 
+              force=args.force, verbose=args.verbose, read=args.read,
+              # IPython methods
+              ip_user_ns=ip.user_ns, 
+              ip_run_cell=ip.run_cell,
+              ip_push=ip.push,
+              )
+        
 
 def load_ipython_extension(ip):
     """Load the extension in IPython."""
--- test_ipycache.py
+++ test_ipycache.py
@@ -7,8 +7,9 @@ Tests for ipycache.
 # Imports
 #------------------------------------------------------------------------------
 import os
-from nose.tools import raises
-from ipycache import save_vars, load_vars, clean_var, clean_vars
+from nose.tools import raises, assert_raises
+from ipycache import (save_vars, load_vars, clean_var, clean_vars, do_save, 
+    cache)
 
 #------------------------------------------------------------------------------
 # Tests
@@ -22,6 +23,23 @@ def test_clean_var():
 def test_clean_vars():
     assert clean_vars(['abc', 'abc,']) == ['abc'] * 2
 
+def test_do_save():
+    path = 'myvars.pkl'
+    
+    # File exists.
+    open(path, 'wb').close()
+    assert_raises(ValueError, do_save, path, force=True, read=True)
+    assert do_save(path, force=True, read=False)
+    assert not do_save(path, force=False, read=False)
+    assert not do_save(path, force=False, read=True)
+    os.remove(path)
+    
+    # File does not exist.
+    assert_raises(ValueError, do_save, path, force=True, read=True)
+    assert do_save(path, force=True, read=False)
+    assert do_save(path, force=False, read=False)
+    assert not do_save(path, force=False, read=True)
+    
 @raises(IOError)
 def test_load_fail():
     path = 'myvars.pkl'
@@ -36,3 +54,4 @@ def test_save_load():
     
     os.remove(path)
     
+
