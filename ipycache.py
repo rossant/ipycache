fa7a03a405d78bf28c16adc33a6305173a6a58c6
Daniel Maturana <dimatura@cmu.edu>
2015-02-06 19:46:01 -0500

    Added gzipped pickle support.

--- ipycache.py
+++ ipycache.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Defines a %%cache cell magic in the notebook to persistent-cache results of 
+"""Defines a %%cache cell magic in the notebook to persistent-cache results of
 long-lasting computations.
 """
 
@@ -9,6 +9,7 @@ long-lasting computations.
 
 # Stdlib
 import inspect, os, sys, textwrap, re
+import gzip
 
 # Our own
 from IPython.config.configurable import Configurable
@@ -23,7 +24,7 @@ from IPython.display import clear_output
 # Six utility functions for Python 2/3 compatibility
 #------------------------------------------------------------------------------
 # Author: "Benjamin Peterson <benjamin@python.org>"
-    
+
 PY2 = sys.version_info[0] == 2
 PY3 = sys.version_info[0] == 3
 
@@ -31,11 +32,11 @@ if PY3:
     import pickle, builtins
     from io import StringIO
     _iteritems = "items"
-    
+
     exec_ = getattr(builtins, "exec")
 else:
     import cPickle as pickle
-    from StringIO import StringIO        
+    from StringIO import StringIO
     _iteritems = "iteritems"
 
     def exec_(_code_, _globs_=None, _locs_=None):
@@ -49,7 +50,7 @@ else:
         elif _locs_ is None:
             _locs_ = _globs_
         exec("""exec _code_ in _globs_, _locs_""")
-    
+
 def iteritems(d, **kw):
     """Return an iterator over the (key, value) pairs of a dictionary."""
     return iter(getattr(d, _iteritems)(**kw))
@@ -85,53 +86,66 @@ def do_save(path, force=False, read=False):
     if force and read:
         raise ValueError(("The 'force' and 'read' options are "
                           "mutually exclusive."))
-         
+
     # Execute the cell and save the variables.
     return force or (not read and not os.path.exists(path))
-    
-def load_vars(path, vars):
-    """Load variables from a pickle file.
-    
-    Arguments:
-    
-      * path: the path to the pickle file.
-      * vars: a list of variable names.
-    
-    Returns:
-    
-      * cache: a dictionary {var_name: var_value}.
-    
-    """
-    with open(path, 'rb') as f:
-        # Load the variables from the cache.
-        try:
-            cache = pickle.load(f)
-        except EOFError as e:
-            raise IOError(e.message)
-        
-        # Check that all requested variables could be loaded successfully
-        # from the cache.
-        missing_vars = sorted(set(vars) - set(cache.keys()))
-        if missing_vars:
-            raise ValueError(("The following variables could not be loaded "
-                "from the cache: {0:s}").format(
-                ', '.join(["'{0:s}'".format(var) for var in missing_vars])))
-        
-        return cache
 
-def save_vars(path, vars_d):
-    """Save variables into a pickle file.
-    
+def load_vars(path, vars, backend):
+    """Load variables from a file.
+
     Arguments:
-    
+
+      * path: the path to the file.
+      * vars: a list of variable names.
+
+    Returns:
+
+      * cache: a dictionary {var_name: var_value}.
+
+    """
+    if backend in ('pkl', 'pkl.gz'):
+        if backend=='pkl':
+            open_fn = open
+        else:
+            open_fn = gzip.open
+
+        with open_fn(path, 'rb') as f:
+            # Load the variables from the cache.
+            try:
+                cache = pickle.load(f)
+            except EOFError as e:
+                raise IOError(e.message)
+
+            # Check that all requested variables could be loaded successfully
+            # from the cache.
+            missing_vars = sorted(set(vars) - set(cache.keys()))
+            if missing_vars:
+                raise ValueError(("The following variables could not be loaded "
+                    "from the cache: {0:s}").format(
+                    ', '.join(["'{0:s}'".format(var) for var in missing_vars])))
+
+            return cache
+    else:
+        raise ValueError('Unknown storage backend {0}'.format(backend))
+
+def save_vars(path, vars_d, backend):
+    """Save variables into a pickle file.
+
+    Arguments:
+
       * path: the path to the pickle file.
       * vars_d: a dictionary {var_name: var_value}.
-    
+
     """
-    with open(path, 'wb') as f:
-        pickle.dump(vars_d, f)
-    
-    
+    if backend=='pkl':
+        with open(path, 'wb') as f:
+            pickle.dump(vars_d, f)
+    elif backend=='pkl.gz':
+        with gzip.open(path, 'wb') as f:
+            pickle.dump(vars_d, f)
+    else:
+        raise ValueError('Unknown storage backend {0}'.format(backend))
+
 #------------------------------------------------------------------------------
 # CapturedIO
 #------------------------------------------------------------------------------
@@ -141,7 +155,7 @@ def save_captured_io(io):
             stderr=StringIO(io._stderr.getvalue()),
             outputs=getattr(io, '_outputs', []), # Only IPython master has this
         )
-        
+
 def load_captured_io(captured_io):
     try:
         return CapturedIO(captured_io.get('stdout', None),
@@ -152,7 +166,7 @@ def load_captured_io(captured_io):
         return CapturedIO(captured_io.get('stdout', None),
                           captured_io.get('stderr', None),
                           )
-                            
+
 class myStringIO(StringIO):
     """class to simultaneously capture and output"""
     def __init__(self, out=None, buf=""):
@@ -171,46 +185,46 @@ class capture_output_and_print(object):
     stdout = True
     stderr = True
     display = True
-    
+
     def __init__(self, stdout=True, stderr=True, display=True):
         self.stdout = stdout
         self.stderr = stderr
         self.display = display
         self.shell = None
-    
+
     def __enter__(self):
         from IPython.core.getipython import get_ipython
         from IPython.core.displaypub import CapturingDisplayPublisher
-        
+
         self.sys_stdout = sys.stdout
         self.sys_stderr = sys.stderr
-        
+
         if self.display:
             self.shell = get_ipython()
             if self.shell is None:
                 self.save_display_pub = None
                 self.display = False
-        
+
         stdout = stderr = outputs = None
         if self.stdout:
             #stdout = sys.stdout = StringIO()
             stdout = sys.stdout = myStringIO(out=IPython.utils.io.stdout)
         if self.stderr:
             #stderr = sys.stderr = StringIO()
-            stderr = sys.stderr = myStringIO(out=self.sys_stderr)            
+            stderr = sys.stderr = myStringIO(out=self.sys_stderr)
         if self.display:
             self.save_display_pub = self.shell.display_pub
             self.shell.display_pub = CapturingDisplayPublisher()
             outputs = self.shell.display_pub.outputs
 
         return CapturedIO(stdout, stderr, outputs)
-    
+
     def __exit__(self, exc_type, exc_value, traceback):
         sys.stdout = self.sys_stdout
         sys.stderr = self.sys_stderr
         if self.display and self.shell:
             self.shell.display_pub = self.save_display_pub
-            
+
 #------------------------------------------------------------------------------
 # %%cache Magics
 #------------------------------------------------------------------------------
@@ -219,13 +233,13 @@ def cache(cell, path, vars=[],
           # without IPython, by giving mock functions here instead of IPython
           # methods.
           ip_user_ns={}, ip_run_cell=None, ip_push=None, ip_clear_output=lambda : None,
-          force=False, read=False, verbose=True):
-    
+          force=False, read=False, verbose=True, backend=None):
+
     if not path:
         raise ValueError("The path needs to be specified as a first argument.")
-    
+
     path = os.path.abspath(path)
-        
+
     if do_save(path, force=force, read=read):
         # Capture the outputs of the cell.
         with capture_output_and_print() as io:
@@ -240,24 +254,24 @@ def cache(cell, path, vars=[],
             cache = {var: ip_user_ns[var] for var in vars}
         except KeyError:
             vars_missing = set(vars) - set(ip_user_ns.keys())
-            vars_missing_str = ', '.join(["'{0:s}'".format(_) 
+            vars_missing_str = ', '.join(["'{0:s}'".format(_)
                 for _ in vars_missing])
             raise ValueError(("Variable(s) {0:s} could not be found in the "
                               "interactive namespace").format(vars_missing_str))
         # Save the outputs in the cache.
         cache['_captured_io'] = save_captured_io(io)
         # Save the cache in the pickle file.
-        save_vars(path, cache)
+        save_vars(path, cache, backend)
         ip_clear_output() # clear away the temporary output and replace with the saved output (ideal?)
         if verbose:
             print("[Saved variables '{0:s}' to file '{1:s}'.]".format(
                 ', '.join(vars), path))
-        
-    # If the cache file exists, and no --force mode, load the requested 
+
+    # If the cache file exists, and no --force mode, load the requested
     # variables from the specified file into the interactive namespace.
     else:
         # Load the variables from cache in inject them in the namespace.
-        cache = load_vars(path, vars)
+        cache = load_vars(path, vars, backend)
         # Handle the outputs separately.
         io = load_captured_io(cache.get('_captured_io', {}))
         # Push the remaining variables in the namespace.
@@ -268,22 +282,22 @@ def cache(cell, path, vars=[],
 
     # Display the outputs, whether they come from the cell's execution
     # or the pickle file.
-    io() # output is only printed when loading file    
+    io() # output is only printed when loading file
 
-        
-    
+
+
 @magics_class
 class CacheMagics(Magics, Configurable):
     """Variable caching.
 
     Provides the %cache magic."""
-    
+
     cachedir = Unicode('', config=True)
-    
+
     def __init__(self, shell=None):
         Magics.__init__(self, shell)
         Configurable.__init__(self, config=shell.config)
-     
+
     @magic_arguments.magic_arguments()
     @magic_arguments.argument(
         'to', nargs=1, type=str,
@@ -310,21 +324,25 @@ class CacheMagics(Magics, Configurable):
         help=("Always read from the file and prevent the cell's execution, "
               "raising an error if the file does not exist.")
     )
+    @magic_arguments.argument(
+        '-b', '--backend',
+        help=("Storage backend: 'pkl', 'pkl.gz'")
+    )
     @cell_magic
     def cache(self, line, cell):
         """Cache user variables in a file, and skip the cell if the cached
         variables exist.
-        
+
         Usage:
-        
+
             %%cache myfile.pkl var1 var2
-            # If myfile.pkl doesn't exist, this cell is executed and 
+            # If myfile.pkl doesn't exist, this cell is executed and
             # var1 and var2 are saved in this file.
             # Otherwise, the cell is skipped and these variables are
             # injected from the file to the interactive namespace.
             var1 = ...
             var2 = ...
-        
+
         """
         ip = self.shell
         args = magic_arguments.parse_argstring(self.cache, line)
@@ -345,10 +363,20 @@ class CacheMagics(Magics, Configurable):
                 except:
                     pass
             path = os.path.join(cachedir, path)
-        cache(cell, path, vars=vars, 
+        # infer storage backend from path if None
+        if args.backend is None:
+            # try to guess, but default is pickle
+            if path.endswith('.pkl') or path.endswith('.pickle'):
+                backend = 'pkl'
+            elif path.endswith('.pkl.gz') or path.endswith('.pickle.gz'):
+                backend = 'pkl.gz'
+            else:
+                backend = 'pkl'
+        cache(cell, path, vars=vars,
               force=args.force, verbose=not args.silent, read=args.read,
+              backend=backend,
               # IPython methods
-              ip_user_ns=ip.user_ns, 
+              ip_user_ns=ip.user_ns,
               ip_run_cell=ip.run_cell,
               ip_push=ip.push,
               ip_clear_output=clear_output
@@ -357,4 +385,4 @@ class CacheMagics(Magics, Configurable):
 def load_ipython_extension(ip):
     """Load the extension in IPython."""
     ip.register_magics(CacheMagics)
-    
+
